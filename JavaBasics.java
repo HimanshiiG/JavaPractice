8ab525f6dd25aa620f422e26d5c0fa594acf8fc4
HimanshiiG <gaurmansi3657@gmail.com>
2024-11-23 22:39:23 +0530

    JavaBasics file

---
+++ JavaBasics.java
@@ -0,0 +1,15 @@
+public class JavaBasics {
+    public static void main (String args[]){
+       int a = 20;
+       int b = 10;
+       System.out.println(a);
+       System.out.println(b);
+    
+       String name ="Himanshi";
+       System.out.println(name);
+       
+
+
+
+    }
+}
